cc52ec06900b713612fec547e81e5659c1bf7f7e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-18 23:05:29 +0000

    Draw blank grid

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
-function love.load()
+local xSize
+local ySize
+local cellSize = 5
+local border = 1
+local cellDrawSize = cellSize - border
 
+function love.load()
+    xSize = love.graphics.getWidth() / cellSize
+    ySize = love.graphics.getHeight() / cellSize
 end
 
 function love.update()
@@ -7,5 +14,9 @@ function love.update()
 end
 
 function love.draw()
-
+    for x = 1, xSize do
+        for y = 1, ySize do
+            love.graphics.rectangle('fill', (x - 1) * cellSize, (y - 1) * cellSize, cellDrawSize, cellDrawSize)
+        end
+    end
 end
