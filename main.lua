d6420f38ad7c1f13b24e305b0f835ba1908a9433
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-02-03 14:24:25 +0000

    Incorporate camera controls

--- main.lua
+++ main.lua
@@ -1,18 +1,25 @@
 local CelAut = require("src.CelAutCaveGen")
+local Camera = require("src.camera")
 
 local xSize
 local ySize
 local cellSize = 5
 local border = 1
 local cellDrawSize = cellSize - border
-local wallChance = 480
+local wallChance = 460
 local caveGen = true
+
+-- Camera variables
+local x, y = 0, 0
+local zoom = 1
 
 local rng = love.math.newRandomGenerator(os.time())
 
 function love.load()
     xSize = love.graphics.getWidth() / cellSize
     ySize = love.graphics.getHeight() / cellSize
+    -- xSize = 110
+    -- ySize = 110
     love.graphics.setBackgroundColor(255, 255, 255, 255)
     love.keyboard.setKeyRepeat(true)
 
@@ -29,18 +36,24 @@ function love.load()
     end
 end
 
-function love.update()
-    selectedX = math.floor(love.mouse.getX() / cellSize) + 1
-    selectedY = math.floor(love.mouse.getY() / cellSize) + 1
+function love.update(dt)
+    selectedX = math.floor((love.mouse.getX() + x )/ cellSize) + 1
+    selectedY = math.floor((love.mouse.getY() + y )/ cellSize) + 1
 
     if love.mouse.isDown(1) and selectedX <= xSize and selectedY <= ySize then
         grid[selectedX][selectedY] = true
     elseif love.mouse.isDown(2) and selectedX <= xSize and selectedY <= ySize then
         grid[selectedX][selectedY] = false
     end
+
+    checkKeyboardInput(dt)
+    Camera:setPosition(x, y)
+    Camera:setScale(zoom, zoom)
+    -- Camera:setRotation(rotation)
 end
 
 function love.draw()
+    Camera:set()
     for x = 1, xSize do
         for y = 1, ySize do
             if x == selectedX and y == selectedY then
@@ -53,8 +66,11 @@ function love.draw()
             love.graphics.rectangle('fill', (x - 1) * cellSize, (y - 1) * cellSize, cellDrawSize, cellDrawSize)
         end
     end
+    Camera:unset()
+    mouse_x = math.floor(Camera.x / cellSize) + selectedX
+    mouse_y = math.floor(Camera.y / cellSize) + selectedY
     love.graphics.setColor(0, 0, 0)
-    love.graphics.print('selected x: ' .. selectedX .. ', selected y: ' .. selectedY)
+    love.graphics.print('selected x: ' .. mouse_x .. ', selected y: ' .. mouse_y)
 end
 
 function love.keypressed(key)
@@ -62,40 +78,79 @@ function love.keypressed(key)
         love.event.quit()
     end
 
-    if caveGen then
-        createCave()
-    else
-        local nextGrid = {}
+    if key == "r" then
 
-        for x = 1, xSize do
-            nextGrid[x] = {}
-            for y = 1, ySize do
-                local neighbourCount = 0
+        if caveGen then
+            createCave()
+        else
+            local nextGrid = {}
 
-                for dx = -1, 1 do
-                    for dy = -1, 1 do
-                        if not (dx == 0 and dy == 0) and grid[x + dx] and grid[x + dx][y + dy] then
-                            neighbourCount = neighbourCount + 1
-                        end
+            for x = 1, xSize do
+                nextGrid[x] = {}
+                for y = 1, ySize do
+                    local neighbourCount = 0
 
-                        if caveGen then
-                            if grid[x + dx] == nil then
+                    for dx = -1, 1 do
+                        for dy = -1, 1 do
+                            if not (dx == 0 and dy == 0) and grid[x + dx] and grid[x + dx][y + dy] then
                                 neighbourCount = neighbourCount + 1
-                            elseif grid[x + dx][y + dy] == nil then
-                                neighbourCount = neighbourCount + 1
+                            end
+
+                            if caveGen then
+                                if grid[x + dx] == nil then
+                                    neighbourCount = neighbourCount + 1
+                                elseif grid[x + dx][y + dy] == nil then
+                                    neighbourCount = neighbourCount + 1
+                                end
                             end
                         end
                     end
+                    nextGrid[x][y] = neighbourCount == 3 or (grid[x][y] and neighbourCount == 2)
                 end
-                nextGrid[x][y] = neighbourCount == 3 or (grid[x][y] and neighbourCount == 2)
             end
-        end
 
-        grid = nextGrid
+            grid = nextGrid
+        end
     end
 end
 
 function createCave()
-    celAut = CelAut.create(xSize, ySize)
+    celAut = CelAut.create(xSize, ySize, wallChance)
     grid = celAut.grid
+end
+
+function checkKeyboardInput(dt)
+    tempX, tempY = 0, 0
+
+    if love.keyboard.isDown("w") then
+        tempY = tempY - Camera.panSpeed * dt
+    end
+
+    if love.keyboard.isDown("s") then
+        tempY = tempY + Camera.panSpeed * dt
+    end
+    
+    if love.keyboard.isDown("a") then
+        tempX = tempX - Camera.panSpeed * dt
+    end
+
+    if love.keyboard.isDown("d") then
+        tempX = tempX + Camera.panSpeed * dt
+    end
+
+    x = x + tempX
+    y = y + tempY
+
+    if love.keyboard.isDown("q") then
+        rotation = rotation + Camera.rotateSpeed * dt
+    end
+    if love.keyboard.isDown("e") then
+        rotation = rotation - Camera.rotateSpeed * dt
+    end
+    if love.keyboard.isDown("z") then
+        zoom = zoom + Camera.zoomSpeed * dt
+    end
+    if love.keyboard.isDown("x") then
+        zoom = zoom - Camera.zoomSpeed * dt
+    end
 end
